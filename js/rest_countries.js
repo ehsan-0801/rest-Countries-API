e00e859feab87dde54e6053601cbc4ab46770df8
ehsan-0801 <ehsanulahmmed@gmail.com>
2022-03-15 17:54:37 +0600

    rest countries api updated

--- js/rest_countries.js
+++ js/rest_countries.js
@@ -9,18 +9,47 @@ const displaycountries = countries =>{
     container.innerHTML = countriesHTML.join(' ');
     console.log(countriesHTML[0]);
 }
-const getCountriesHTML = country =>{
+// const getCountriesHTML = country =>{
+//     const {name, flags} = country;
+//     return `
+//     <div class="card-group col-md-4">
+//   <div class="card m-md-3 border border-5 rounded">
+//     <img style="width:200px; height:100px" src="${flags.png}" class="card-img-top" alt="...">
+//     <div class="card-body">
+//       <h5 class="card-title">${name.common}</h5>
+//       <p class="card-text"></p>
+//       <p class="card-text"><small class="text-muted"></small></p>
+//     </div>
+//   </div>
+//   </div>
+//     `;
+// }
+const getCountriesHTML = ({name, flags,area,region}) =>{
     return `
     <div class="card-group col-md-4">
   <div class="card m-md-3 border border-5 rounded">
-    <img style="width:200px; height:100px" src="${country.flags.png}" class="card-img-top" alt="...">
+    <img style="width:200px; height:100px" src="${flags.png}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${country.name.common}</h5>
-      <p class="card-text"></p>
-      <p class="card-text"><small class="text-muted"></small></p>
+      <h5 class="card-title">${name.common}</h5>
+      <p class="card-text">${area} km</p>
+      <p class="card-text"><small class="text-muted">${region}</small></p>
     </div>
   </div>
   </div>
     `;
 }
+// const getCountriesHTML = country =>{
+//     return `
+//     <div class="card-group col-md-4">
+//   <div class="card m-md-3 border border-5 rounded">
+//     <img style="width:200px; height:100px" src="${country.flags.png}" class="card-img-top" alt="...">
+//     <div class="card-body">
+//       <h5 class="card-title">${country.name.common}</h5>
+//       <p class="card-text"></p>
+//       <p class="card-text"><small class="text-muted"></small></p>
+//     </div>
+//   </div>
+//   </div>
+//     `;
+// }
 loadCountries()
